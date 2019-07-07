d09c2358870cba2e6e2ca584fa033f54191e738b
Emily Morehouse <emily@cuttlesoft.com>
2019-07-07 12:54:48 -0600

    ✨ (Custom Aliases - iOS Sim Quickstarts) Add aliases to quickly open a simulator for an iOS device

--- zsh-custom/aliases.plugin.zsh
+++ zsh-custom/aliases.plugin.zsh
@@ -34,6 +34,18 @@ alias platform_reset='ios_reset && android_reset'
 alias ios_reset='ionic platform rm ios && ionic platform add ios'
 alias android_reset='ionic platform rm android && ionic platform add android'
 
+# Simulator Quick Starts
+alias ios-simulator="open /Applications/Xcode.app/Contents/Developer/Applications/Simulator.app/"
+alias sim-iphone-x="ios-sim start --devicetypeid 'iPhone-X, 12.1'"
+alias sim-iphone-xs="ios-sim start --devicetypeid 'iPhone-XS, 12.1'"
+alias sim-iphone-xs-max="ios-sim start --devicetypeid 'iPhone-XS-Max, 12.0'"
+alias sim-iphone-8="ios-sim start --devicetypeid 'iPhone-8, 12.0'"
+alias sim-iphone-8-plus="ios-sim start --devicetypeid 'iPhone-8-Plus, 12.0'"
+alias sim-ipad-pro-sm="ios-sim start --devicetypeid 'iPad-Pro--9-7-inch-, 12.0'"
+alias sim-ipad-pro="ios-sim start --devicetypeid 'iPad-Pro--12-9-inch---2nd-generation-, 12.0'"
+alias android-tablet="emulator -avd Nexus_7_API_25 &"
+alias android-pixel="emulator -avd Pixel_XL_API_25 &"
+
 # Project Quick Starts
 alias ws='cl ~/Development/CuttleDev/Projects/Clients/WeatherSTEM/weatherstem.mobile && subl . && ionic serve'
 
